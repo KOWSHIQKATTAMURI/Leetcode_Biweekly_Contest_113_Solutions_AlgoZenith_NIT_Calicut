9ceca485ea0b0da850a8769327156104ac5706b6
Kattamuri Kowshiq <kattamuri_b201298bt@nitc.ac.in>
2023-09-17 13:25:50 +0530

    Create 1st_question_Minimum_Right_Shifts_to_Sort_the_Array.cpp

---
+++ 1st_question_Minimum_Right_Shifts_to_Sort_the_Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumRightShifts(vector<int>& nums){
+        int n = nums.size();
+        
+        //to store the number of instances where the present element is greater than the next element as well as the index of that instance
+        int number_of_break_points = 0, break_point_index = 0;
+        for(int i = 0; i < n - 1; i++){
+            if(nums[i] > nums[i + 1]){
+                number_of_break_points++;
+                break_point_index = i + 1;
+            }
+        }
+        
+        //if there are zero instances, then we need not sort the array as the array is already sorted
+        //if the number of instances is greater than one, that means there are more than one point where the present element is greater than its next element. So right shifting it does not make it sorrted.
+        //also if the right most element is greater than the left most element (but number of break points is not equal to 0), that cannot be sorted. So in both the cases we return -1.
+        if(number_of_break_points == 0){
+            return 0;
+        }else if((number_of_break_points > 1) or (nums[n - 1] > nums[0])){
+            return -1;
+        }
+        return n - break_point_index;
+    }
+};
